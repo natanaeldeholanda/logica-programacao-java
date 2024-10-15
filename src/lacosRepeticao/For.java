9f4b06f38962fbfd038fc7651340d60fce7333b6
Natanael de Holanda e Oliveira <natan.holanda@gmail.com>
2024-10-15 16:16:16 -0300

    Parei no vídeo de Lógica Virado no Jiraya 34 - For pt 02

--- src/lacosRepeticao/For.java
+++ src/lacosRepeticao/For.java
@@ -1,15 +1,13 @@
 package lacosRepeticao;
 
 /*
-Elabore uma rotina que apresente todos os números pares na faicha de o a 100
+Elabore uma rotina que apresente todos os números pares na faixa de 0 a 100
  */
 
 public class For {
     public static void main(String[] args) {
-
-        int contator = 0;
-        for(contator = 0; contator <= 100; contator++){
-            if(contator % 2 == 0){
+        for (int contator = 0; contator <= 100; contator++) {
+            if (contator % 2 == 0) {
                 System.out.println(contator);
             }
         }
