911d19a9b843c0b9f707fa28c4dd092df423b814
Natanael de Holanda e Oliveira <natan.holanda@gmail.com>
2024-10-17 16:30:05 -0300

    refactor o projeto para Conclusão do curso e iniciando o MaratonaJava.

--- src/logicaProgramacao/SequenciaScape.java
+++ src/logicaProgramacao/SequenciaScape.java
@@ -1,6 +1,6 @@
 package logicaProgramacao;
 
-public class sequenciaScape {
+public class SequenciaScape {
     public static void main(String[] args) {
        int idade = 45;
         System.out.println(idade + " é a idade dele.");
